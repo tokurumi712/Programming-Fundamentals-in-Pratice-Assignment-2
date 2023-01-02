32058cd091713a306fa6fe1d37a33166b2cee6e0
tokurumi712 <121742626+tokurumi712@users.noreply.github.com>
2023-01-02 19:35:10 +0800

    Add files via upload

---
+++ readme.java
@@ -0,0 +1,22 @@
+public class readme {
+    /*
+    Name: DeYi Tian
+    StudentID: 202283930024
+    Class: Software Engineering Class 1
+    description: The basic functionality should have been implemented.
+                 But I encountered some minor problems in the implementation process,
+                 which were finally solved by asking my classmates for advice.
+                 For example£ºStringBuilder sb = new StringBuilder(); ...sb.append...;
+                 I always had bugs when I used the previous method,
+                 but after consulting XiangTeng SiMa , I learned a simpler method without errors.
+
+     */
+
+
+
+
+
+
+
+
+}
